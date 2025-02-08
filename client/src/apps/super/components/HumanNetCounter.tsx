1957562c4829a5effe94784e35ce94712b81fc05
koro <drganeelmegumin23@gmail.com>
2025-02-08 12:56:32 -0800

    human button works in backend :3

--- client/src/apps/super/components/HumanNetCounter.tsx
+++ client/src/apps/super/components/HumanNetCounter.tsx
@@ -1,30 +1,84 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
+import { Dispatch, SetStateAction } from "react";
+import { SuperScores } from "../SuperApp";
 
-const HumanButton: React.FC = () => {
-  // Declare a state variable to keep track of the count
-  const [count, setCount] = useState<number>(0);
-  const [failCount, setFailCount] = useState<number>(0);
+  type countKeys = keyof SuperScores;
 
-  // Function to increment the count by 1
-  const increment = () => setCount(count + 1);
-  const failIncrement = () => setFailCount(failCount+1)
 
-  return (
-    <div className="py-5 my-10 text-white text-4xl"> 
-    Human Net:
-    <br/>
-    <button
-      className="mt-10 p-10 mx-4 bg-green-500 text-white text-2xl rounded"
-      onClick={increment}>
-      Success: {count}
-    </button>
-    <button
-      className="mt-10 p-10 mx-4 bg-red-500 text-white text-2xl rounded"
-      onClick={failIncrement}>
-      Failed: {failCount}
-    </button>
-    </div>
+  function HumanButton({
+    // Declare a state variable to keep track of the count
+    handleCount,
+    successKey,
+    failKey,
+    count,
+    className,
+  
+  }:{
+    handleCount: (key: countKeys) => void;
+    successKey: countKeys;
+    failKey: countKeys;
+    count: SuperScores;
+    className?:string;
+  }){
+   return (
+    <>
+      <button
+        // className="mt-10 p-10 mx-4 bg-green-500 text-white text-2xl rounded"
+        onClick={() => handleCount(successKey)}
+        className={className}
+        id='one'>
+          <p>
+            {count[successKey]}
+          </p>
+     </button>
+     <button
+        className={className}
+        onClick={() => handleCount(failKey)}
+        id='one'>
+          <p>
+            {count[failKey]}
+          </p>
+     </button>
+    </>
+   ); 
+  };
+
+function HumanCounter({
+    count, 
+    className,
+    setCount,
+     
+  }:{
+    
+    count: SuperScores;
+    className?:string;
+    setCount:Dispatch<SetStateAction<SuperScores>>;
+    
+  }){
+
+    const handleCount = (key: countKeys) => {
+          setCount(prevCount => ({
+              ...prevCount,
+              [key]: prevCount[key] + 1,
+          }));
+    //  }
+  };
+
+return(
+    <>
+      <HumanButton
+        handleCount={handleCount}
+        successKey='Success'
+        failKey='Failed'
+        count={count}
+        className={className}
+      />
+    </>
   );
+
+
 };
 
-export default HumanButton;
+export default HumanCounter;
+
+//mt-10 p-10 mx-4 bg-red-500 text-white text-2xl rounded
