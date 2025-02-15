11890c70889181f1269d291f033f6fbcc6528eb7
kinnon <mukoreii@gmail.com>
2025-02-14 19:22:03 -0800

    Adjusted the buttons (mario)

--- client/src/apps/match/MatchApp.tsx
+++ client/src/apps/match/MatchApp.tsx
@@ -351,7 +351,7 @@ function MatchApp() {
                         color='black'
                         className='snap-none'
                     />
-                </button>
+</button>
 
                 <div className={`fixed right-4 top-4 z-20 flex flex-row gap-3 rounded-md p-1`}>
                     <ToggleButton
@@ -438,10 +438,10 @@ function MatchApp() {
                     unselectClassName='bg-red-300 absolute left-[7.6em] top-[48em] h-[1em] w-[4em] rotate-90'></CoralSectionButton>
                     <CoralSectionButton selectClassName='bg-green-300 absolute left-[14.25em] top-[48.3em] h-[1em] w-[4em] rotate-90'
                     unselectClassName='bg-red-300 absolute left-[14.25em] top-[48.3em] h-[1em] w-[4em] rotate-90'></CoralSectionButton>
-                    <CoralSectionButton selectClassName='bg-green-300 absolute left-[17.2em] top-[45em] h-[1em] w-[4em] rotate-[2.64rad]'
-                    unselectClassName='bg-red-300 absolute left-[17.2em] top-[45em] h-[1em] w-[4em] rotate-[2.64rad]'></CoralSectionButton>
-                    <CoralSectionButton selectClassName='bg-green-300 absolute left-[20.7em] top-[51.1em] h-[1em] w-[4em] rotate-[2.64rad]'
-                    unselectClassName='bg-red-300 absolute left-[20.7em] top-[51.1em] h-[1em] w-[4em] rotate-[2.64rad]'></CoralSectionButton>
+                    <CoralSectionButton selectClassName='bg-green-300 absolute left-[9.1em] top-[45em] h-[1em] w-[4em] rotate-[2.64rad]'
+                    unselectClassName='bg-red-300 absolute left-[9.1em] top-[45em] h-[1em] w-[4em] rotate-[2.64rad]'></CoralSectionButton>
+                    <CoralSectionButton selectClassName='bg-green-300 absolute left-[12.5em] top-[51.1em] h-[1em] w-[4em] rotate-[2.64rad]'
+                    unselectClassName='bg-red-300 absolute left-[12.5em] top-[51.1em] h-[1em] w-[4em] rotate-[2.64rad]'></CoralSectionButton>
                     
                 <p
                     className={`text-center m-5 ${toggleState ? 'text-white' : 'text-[#171c26]'}`}>
