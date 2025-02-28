0dfe5d62d1b98d18f20ca931355adeb8a60cc320
mossboss64 <clareemarie0012@gmail.com>
2025-02-28 10:58:39 -0800

    WIP pit changes
    computer bouta die smh

--- client/src/apps/pit/PitApp.tsx
+++ client/src/apps/pit/PitApp.tsx
@@ -9,7 +9,8 @@ import ConeStacker from '../../components/ConeStacker';
 import { usePreventUnload } from '../../lib/usePreventUnload';
 import ImageUploader from './components/ImageUploader';
 import { useFetchJson } from '../../lib/useFetch';
-import { postJson } from '../../lib/postJson';
+import { useQueue } from '../../lib/useQueue';
+import ToggleButton from '../../components/LightVDarkMode';
 
 function PitApp() {
     usePreventUnload();
@@ -18,12 +19,11 @@ function PitApp() {
         '/data/pit/scouted-teams'
     );
 
-    const [sending, setSending] = useState(false);
-
     const [additionalNotes, setAdditionalNotes] = useState('');
     const [batteryNumber, setBatteryNumber] = useState(Number);
     const [teamNumber, setTeamNumber] = useState(Number);
-
+    const [toggleState, setToggleState] = useState(false);
+    const [sendQueue, sendAll, queue, sending] = useQueue();
     const [scouterName, setScouterName] = useState('');
     const [robotImage, setRobotImage] = useState('');
     useEffect(() => {
@@ -41,27 +41,25 @@ function PitApp() {
             photo: robotImage,
             comments: additionalNotes,
         };
-
-        setSending(true);
-        try {
-            const result = await postJson('/data/pit', data);
-            if (!result.ok) throw new Error('Request Did Not Succeed');
+            sendQueue('/data/pit', data);
             refreshScoutedTeams();
             setBatteryNumber(0);
             setAdditionalNotes('');
             setTeamNumber(0);
             setRobotImage('');
-        } catch {
-            alert('Sending Data Failed');
-        }
-        setSending(false);
     };
 
     const inputBattery = {
         width: '150px',
         height: '50px',
     };
-
+    function buttonToggle() {
+        if (toggleState == false) {
+            setToggleState(true);
+        } else {
+            setToggleState(false);
+        }
+    }
 
     return (
         <>
@@ -108,6 +106,21 @@ function PitApp() {
                         )}
                     </Dialog>
                     <ConeStacker />
+                    <div className={`fixed right-4 top-4 z-20 flex flex-row gap-3 rounded-md p-1`}>
+                        <ToggleButton
+                            className='' 
+                            buttonClassName=''
+                            onClick={buttonToggle}>
+                        <MaterialSymbol
+                            icon={`${toggleState ? 'dark_mode' : 'light_mode'}`}
+                            size={60}
+                            fill
+                            grade={200}
+                            color='#48c55c'
+                            className='snap-none'
+                        />
+                        </ToggleButton>
+                </div>
                 </div>
 
                 <div className='mb-2 flex items-center justify-center'>
@@ -159,6 +172,15 @@ function PitApp() {
                     className='border-1 pad mx-auto !flex w-min place-content-center rounded-lg border border-gray-700 bg-[#48c55c] px-4 py-4 font-sans text-4xl font-semibold text-black shadow-xl md:bg-opacity-50 '>
                     {sending ? 'Sending...' : 'Submit'}
                 </button>
+                <div>
+                <div className={`${toggleState ? 'text-white' : 'text-[#171c26]'} justify-center text-center`}>
+                Queue: {queue.length}</div>
+                <button
+                    onClick={sendAll}
+                    className='rounded-md bg-amber-500 px-2 py-1 text-center mb-5'>
+                    {sending ? 'Sending...' : 'Resend All'}
+                </button>
+            </div>
             </div>
         </>
     );
