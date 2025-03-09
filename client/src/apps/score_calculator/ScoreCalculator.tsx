07a1b36786301f42ae33598fd3ccb0833fabc8a4
Christian R <cool9373guy@gmail.com>
2025-03-09 17:48:01 -0400

    adjustment
    more like TBA

--- client/src/apps/score_calculator/ScoreCalculator.tsx
+++ client/src/apps/score_calculator/ScoreCalculator.tsx
@@ -190,7 +190,7 @@ function ScoreCalculator() {
                     </div>
                 </div>
                 
-                <div className='grid grid-cols-12 justify-center py-2 bg-gray-800 text-black-100 text-md'>
+                <div className='grid grid-cols-12 justify-center py-2 bg-gray-800 text-black-100 text-xl'>
                 <p className='col-span-3 border-green-800 border-4 bg-green-400 px-3 py-2 text-center'>Auto Leave</p>
                     <p className='col-span-9 border-green-800 border-4 bg-green-300 px-3 py-2 text-center'>Points: {autoLeave*3}</p>
                 <p className='col-span-3 border-green-800 border-4 bg-green-500 px-3 py-2 text-center'>Auto Coral</p>
@@ -202,10 +202,10 @@ function ScoreCalculator() {
                 <p className='col-span-3 border-green-900 border-4 bg-green-400 px-3 py-2 text-center font-bold'>Total Auto</p>
                     <p className='col-span-9 border-green-900 border-4 bg-green-300 px-3 py-2 text-center font-bold'>Points: {autoPoints}</p>
                     <p className='col-span-3 border-green-800 border-4 bg-green-500 px-3 py-2 text-center'>Teleop Coral</p>
-                    <p className='col-span-1 border-green-800 border-4 bg-green-400 px-3 py-2'>L1: {teleCoral1}</p>
-                    <p className='col-span-1 border-green-800 border-4 bg-green-400 px-3 py-2'>L2: {teleCoral2}</p>
-                    <p className='col-span-1 border-green-800 border-4 bg-green-400 px-3 py-2'>L3: {teleCoral3}</p>
-                    <p className='col-span-1 border-green-800 border-4 bg-green-400 px-3 py-2'>L4: {teleCoral4}</p>
+                    <p className='col-span-1 border-green-800 border-4 bg-green-400 px-3 py-2'>L1: {teleCoral1+autoCoral1}</p>
+                    <p className='col-span-1 border-green-800 border-4 bg-green-400 px-3 py-2'>L2: {teleCoral2+autoCoral2}</p>
+                    <p className='col-span-1 border-green-800 border-4 bg-green-400 px-3 py-2'>L3: {teleCoral3+autoCoral3}</p>
+                    <p className='col-span-1 border-green-800 border-4 bg-green-400 px-3 py-2'>L4: {teleCoral4+autoCoral4}</p>
                     <p className='col-span-5 border-green-800 border-4 bg-green-400 px-3 py-2 text-center'>Points: {teleCoralTotal}</p>
                 <p className='col-span-3 border-green-800 border-4 bg-green-400 px-3 py-2 text-center'>Algae</p>
                     <p className='col-span-2 border-green-800 border-4 bg-green-300 px-3 py-2'>Net: {teleAlgaeNet+autoAlgaeNet}</p>
